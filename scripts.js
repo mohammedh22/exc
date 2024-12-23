88de8bb081cfa13a6cea7cd9a799834cfad4156d
mohammedh22 <mdharbi@outlook.sa>
2024-12-23 22:20:38 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function addNumbers() {
+    
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Please enter valid numbers.");
+    } 
+    
+    else {var sum = num1 + num2;
+
+        
+        document.getElementById("result").innerHTML = "The result is: " + sum;
+    }
+}
